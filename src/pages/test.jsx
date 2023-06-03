98bb228856972d3b05be87a5afdf9469b7e9a638
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-06-03 22:12:32 +0700

    features: add crud module

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,18 +1,6 @@
-import { Modal } from "@/components";
 import { CrudModule } from "@/modules";
-import { useState } from "react";
 
 export const TestPage = () => {
-  const [showModal, setShowModal] = useState(false);
-  const onClose = () => {
-    setShowModal(false);
-  };
-
-  const onSubmit = () => {
-    setShowModal(false);
-    console.log("Accept");
-  };
-
   return (
     <div className="flex h-screen w-full justify-center items-center">
       <CrudModule />
